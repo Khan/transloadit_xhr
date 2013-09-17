a575017ab182899c76500c4019bf964bbfb67510
Pamela Fox <pamela.fox@gmail.com>
2013-09-17 13:40:00 -0700

    Use SSL

--- transloadit_xhr.js
+++ transloadit_xhr.js
@@ -57,7 +57,7 @@
         formPost.append("file", file);
 
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://api2.transloadit.com/assemblies", true);
+        xhr.open("POST", "https://api2.transloadit.com/assemblies", true);
 
         xhr.onreadystatechange = function(event) {
             var req = event.target;
