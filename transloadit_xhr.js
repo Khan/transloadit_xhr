668876125bb6058c2210f6a8c57a351ce7c05da6
Pamela Fox <pamela.fox@gmail.com>
2013-10-30 10:20:10 -0700

    Update transloadit_xhr.js

--- transloadit_xhr.js
+++ transloadit_xhr.js
@@ -16,7 +16,7 @@
         // To avoid Chrome blocking non-secure script
         assemblyUrl = assemblyUrl.replace(/^http:/, 'https:');
         // To avoid SSL certificate issue on api2.jingyi 
-        assemblyUrl = assemblyUrl.replace(/api2.jingyi, 'api2');
+        assemblyUrl = assemblyUrl.replace(/api2.jingyi/, 'api2');
         
         $.ajax({
             url: assemblyUrl,
